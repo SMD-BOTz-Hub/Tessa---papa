4d97cc697a543b97d3daf2c9e6da99056f10b3b1
Samar khan <149138560+SMD-BOTz-Hub@users.noreply.github.com>
2024-01-29 22:43:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /SMD_TESSA_FILTER
 fi
-cd /Rocorr
+cd /SMD_TESSA_FILTER
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
